6030e2b3e22558fc7b880876586566baa067784c
Divya Abilash <divya@Abilashs-MacBook-Pro.local>
2016-02-27 23:10:07 -0800

    Quiz Project

---
+++ QuizBooks/questions.js
@@ -0,0 +1,275 @@
+var Geography = [{
+    question: "Where is the great Barrier Reef?",
+    questionType: 1,
+    choices: ["Africa", "N.America", "S.America", "Asia", "Australia"],
+    correctChoice: "Australia",
+    score: 5
+
+}, {
+    question: "On planet Earth, there is no centrifugal force at the",
+    questionType: 1,
+    choices: ["Poles", "Tropic of Capricorn", "Equator", "Tropic of Cancer", "None of these"],
+    correctChoice: "Poles",
+    score: 5
+
+}, {
+    question: "Which among the following planets is smaller in size than the Earth?",
+    questionType: 1,
+    choices: ["Saturn", "Neptune", "Equator", "Uranus", "Venus"],
+    correctChoice: "Venus",
+    score: 5
+
+}, {
+    question: "What planet is Curiosity Rover on now?",
+    questionType: 2,
+    correctAnswer: "Mars",
+    score: 10
+}, {
+    question: "Which is the second largest planet in the solar system?",
+    questionType: 2,
+    correctAnswer: "Saturn",
+    score: 10
+}, {
+    question: "Earth is known as the 'Blue planet' due to--",
+    questionType: 1,
+    choices: ["Reflection", "Presence of huge amount of water on it", "Scattering", "Refraction"],
+    correctChoice: "Presence of huge amount of water on it",
+    score: 5
+
+}, {
+    question: "Apart from water, what runs through the mouth of the River Amazon and Lake Victoria?",
+    questionType: 2,
+    correctAnswer: "Equator",
+    score: 10
+
+}, {
+    question: "Where would you find the Queen Alexandra, Queen Elizabeth and Queen Maud mountain ranges?",
+    questionType: 1,
+    choices: ["Saturn", "Antarctica", "UK", "USA", "Asia"],
+    correctChoice: "Antarctica",
+    score: 5
+}, {
+    question: "Where are the North Yolla Bolly mountains?",
+    questionType: 2,
+    correctAnswer: "USA, California",
+    score: 10
+
+}, {
+    question: "Which sea was known as South Sea?",
+    questionType: 1,
+    choices: ["Indian Ocean", "Antarctica", "Artic Ocean", "Pacific Ocean"],
+    correctChoice: "Pacific Ocean",
+    score: 10
+}];
+
+
+var Maths = [{
+    question: "The statement 'If x is divisible by 8, then it is divisible by 6' is false if x equals",
+    questionType: 1,
+    choices: ["6", "14", "32", "48", "0"],
+    correctChoice: "32",
+    score: 5
+
+}, {
+    question: "In a class of 50 students, 18 take music, 26 take art, and 2 take both art and music. How many students in the class are not enrolled in either music or art?",
+    questionType: 1,
+    choices: ["6", "8", "16", "24", "12"],
+    correctChoice: "8",
+    score: 5
+
+}, {
+    question: "Which equation is an illustration of the additive identity property?",
+    questionType: 1,
+    choices: ["(x)*(1) = x", "x + 0 = x", "x - x = 0", "(x)*(1/x) = 1"],
+    correctChoice: "x + 0 = x",
+    score: 10
+
+}, {
+    question: "Linda paid $48 for a jacket that was on sale for 25% of the original price. What was the original price of the jacket?",
+    questionType: 2,
+    correctAnswer: "$192",
+    score: 10
+
+}, {
+    question: "In a hockey league, 87 players play on seven different teams. Each team has at least 12 players. What is the largest possible number of players on any one team?",
+    questionType: 2,
+    correctAnswer: "3",
+    score: 10
+}, {
+    question: "What does the square root of 144 equal?",
+    questionType: 2,
+    correctAnswer: "12",
+    score: 10
+}, {
+    question: "What is the next number in the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ?",
+    questionType: 2,
+    correctAnswer: "55",
+    score: 10
+}, {
+    question: "Opposite angles of a parallelogram are equal.",
+    questionType: 1,
+    choices: ["True", "False"],
+    correctChoice: "True",
+    score: 10
+}, {
+    question: "What comes after a million, billion and trillion?",
+    questionType: 2,
+    correctAnswer: "quadrillion",
+    score: 10
+}, {
+    question: "How many sides does a nonagon have?",
+    questionType: 1,
+    choices:["6", "8", "9", "24", "12"],
+    score: 10
+}];
+
+var Science = [{
+    question: "When a gas is turned into a liquid, the process is called",
+    questionType: 1,
+    choices: ["condensation", "evaporation", "deposition", "sublimation", "Liquidification"],
+    correctChoice: "condensation",
+    score: 5
+
+}, {
+    question: "Which of the following parts of the sun is easily visible only during a total solar eclipse? ",
+    questionType: 1,
+    choices: ["core", "photosphere", "sunspots", "corona", "moon"],
+    correctChoice: "corona",
+    score: 5
+
+}, {
+    question: "Which of the following elements is a metal ",
+    questionType: 1,
+    choices: ["S", "Se", "I", "Ga", "O"],
+    correctChoice: "Ga",
+    score: 5
+
+}, {
+    question: "Pollination by birds is called",
+    questionType: 2,
+    correctAnswer: "ornithophily",
+    score: 10
+
+}, {
+    question: "The fastest-running terrestrial animal is",
+    questionType: 2,
+    correctAnswer: "cheetah",
+    score: 5
+
+}, {
+    question: "The branch of medical science which is concerned with the study of disease as it affects a community of people is called ",
+    questionType: 2,
+    correctAnswer: "epidemiology",
+    score: 10
+
+},{
+    question: "what is diameter of earth?",
+    questionType: 2,
+    correctAnswer: "8,000 miles",
+    score: 5
+}];
+
+var Computer = [{
+    question: "BIOS stands for Basic Input/Output System",
+    questionType: 1,
+    choices: ["True", "False"],
+    correctChoice: "True",
+    score: 5
+
+}, {
+    question: "What is the permanent memory built into your computer called?",
+    questionType: 1,
+    choices: ["CPU", "CD-ROM", "ROM", "RAM", "Vent"],
+    correctChoice: "ROM",
+    score: 5
+
+}, {
+    question: "What does RAM stand for?",
+    questionType: 1,
+    choices: ["Random Authorization Mechanism", "Readily Accessed Mailer", "  Remote Authorization Mechanism", "Random Access Memory"],
+    correctChoice: "Random Access Memory",
+    score: 5
+
+}, {
+    question: "CD-ROM stands for",
+    questionType: 2,
+    correctAnswer: "Compact Disc Read Only Memory",
+    score: 5
+
+}, {
+    question: "ALU is",
+    questionType: 2,
+    correctAnswer: "Arithmetic Logic Unit",
+    score: 10
+
+}, {
+    question: "VGA is",
+    questionType: 2,
+    correctAnswer: "Video Graphics Array",
+    score: 10
+
+},{
+    question: "Vaccum tubes was used in ___________________ generation",
+    questionType: 1,
+    choices: ["First", "Third", "Fourth", "Fifth"],
+    correctChoice: "First",
+    score: 5
+},{
+    question: "Find Odd Man out",
+    questionType: 1,
+    choices: ["FTP", "POP", "TCP", "SAP"],
+    correctChoice: "POP",
+    score: 5
+}];
+
+var weirdfacts = [{
+    question: "What countries people had the longest life expectation?",
+    questionType: 1,
+    choices: ["IceLand", "Australia", "Greece", "Asia"],
+    correctChoice: "IceLand",
+    score: 5
+}, {
+    question: "What fish can hold objects in its tail?",
+    questionType: 2,
+    correctAnswer: "Sea Horse",
+    score: 5
+}, {
+    question: "The guillotine was invented for chopping off what?",
+    questionType: 2,
+    correctAnswer: "Hands",
+    score: 5
+}, {
+    question: "What did Spanish scientists fit to cows to increase milk yield?",
+    questionType: 1,
+    choices: ["Increase Feed to Cow", "Genetically Modify Cows For More Milk", "Fitted False Teeth", "Make them wander More"],
+    correctChoice: "IceLand",
+    score: 5
+},{
+    question: "How many days a cat usually stay in heat?",
+    questionType: 1,
+    choices: ["6", "8", "9", "5", "12"],
+    correctChoice: "IceLand",
+    score: 5
+},{
+    question: "What color is Absynth?",
+    questionType: 2,
+    correctAnswer: "Green",
+    score: 10
+},{
+    question: "What flavour is Cointreau?",
+    questionType: 2,
+    correctAnswer: "Orange",
+    score: 10
+},{
+    question: "Who averaged one patent for every three weeks of his life?",
+    questionType: 2,
+    correctAnswer: "Thomos Edison",
+    score: 10
+},{
+    question: "What national holiday in Mexico has picnikers munching chocolate coffins and sugar skulls??",
+    questionType: 2,
+    correctAnswer: "The Day of the Dead",
+    score: 10
+}];
+
+var subjects = ['Geography', 'Maths', 'Science', 'Computer', 'weirdfacts'];
